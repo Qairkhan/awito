2e4088d6e3eb7411965e5cff39caf0be1cc73700
qairkhan <shyngys94@gmail.com>
2020-07-07 21:15:01 +0300

    install modal widow

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,8 @@
 
 
 const modalAdd = document.querySelector('.modal__add'),
-addAd = document.querySelector('.add__ad');
+    addAd = document.querySelector('.add__ad');
+
+addAd.addEventListener('click', () => {
+    modalAdd.classList.remove('hide');
+});
