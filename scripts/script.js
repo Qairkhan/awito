fe3e2e52b9ba637715e43d1bc5424df038d65a63
Qairkhan <shyngys94@gmail.com>
2020-07-10 16:26:06 +0300

    modal item3

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,31 @@
-'use strict';
+"use strict";
 
+const modalAdd = document.querySelector(".modal__add"),
+  addAd = document.querySelector(".add__ad"),
+  modalBtnSubmit = document.querySelector(".modal__btn-submit"),
+  modalSubmit = document.querySelector(".modal__submit"),
+  catalog = document.querySelector(".catalog"),
+  modalItem = document.querySelector(".modal__item");
 
-const modalAdd = document.querySelector('.modal__add'),
-    addAd = document.querySelector('.add__ad'),
-    modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-    modalSubmit = document.querySelector('.modal__submit'),
-    catalog = document.querySelector('.catalog'),
-    modalItem = document.querySelector('.modal__item');
-
-const closeModal = (event)=> {
-    const target = event.target;
-    if (target.closest('modal__close') || 
-        target === modalAdd ||
-        target === modalItem) {
-        modalAdd.classList.add('hide');
-        modalItem.classList.add('hide');
-        modalSubmit.reset();
-    } 
+const closeModal = function (event) {
+  const target = event.target;
+  if (target.closest("modal__close") || target === this) {
+    this.classList.add("hide");
+    modalSubmit.reset();
+  }
 };
 
-addAd.addEventListener('click', () => {
-    modalAdd.classList.remove('hide'),
-    modalBtnSubmit.disabled = true;
+addAd.addEventListener("click", () => {
+  modalAdd.classList.remove("hide");
+  modalBtnSubmit.disabled = true;
 });
 
-modalAdd.addEventListener('click', closeModal);
+modalAdd.addEventListener("click", closeModal);
+modalItem.addEventListener("click", closeModal);
 
-catalog.addEventListener('click', event => {
-    const target = event.target;
-    if (target.closest('.card')) {
-        modalItem.classList.remove('hide');
-    }
+catalog.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.closest(".card")) {
+    modalItem.classList.remove("hide");
+  }
 });
