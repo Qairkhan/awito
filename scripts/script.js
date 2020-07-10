45c16032486826989cb55d52248f8f3715836b04
Qairkhan <shyngys94@gmail.com>
2020-07-10 16:02:52 +0300

    modal item

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,9 @@
 const modalAdd = document.querySelector('.modal__add'),
     addAd = document.querySelector('.add__ad'),
     modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-    modalSubmit = document.querySelector('.modal__submit');
+    modalSubmit = document.querySelector('.modal__submit'),
+    catalog = document.querySelector('.catalog'),
+    modalItem = document.querySelector('.modal__item');
 
 addAd.addEventListener('click', () => {
     modalAdd.classList.remove('hide'),
@@ -19,4 +21,8 @@ modalAdd.addEventListener('click', (event)=> {
         modalSubmit.reset();
     }
 
+});
+
+catalog.addEventListener('click', event => {
+    const target = event.target;
 });
